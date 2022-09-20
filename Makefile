93c939d79f2e02965ed71cc966ebd4b305774f19
kta76 <kta76@csil-cpu5.cs.surrey.sfu.ca>
2022-09-20 14:19:14 -0700

    transfer

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CC=gcc
 # enable debug symbols and warnings
 CFLAGS=-c -Wall
 
-all: server client
+all: server client tunnel
 
 server: server.o
 	$(CC) server.o -o server
@@ -12,12 +12,17 @@ server: server.o
 client: client.o
 	$(CC) client.o -o client
 
+tunnel: tunnel.o
+	$(CC) tunnel.o -o tunnel
+
 server.o: server.c
 	$(CC) $(CFLAGS) server.c
 
 client.o: client.c
 	$(CC) $(CFLAGS) client.c
 
+tunnel.o: tunnel.c
+	$(CC) $(CFLAGS) tunnel.c
 clean:
-	rm -rf *.o client server
+	rm -rf *.o client server tunnel
 
--- client.c
+++ client.c
@@ -20,34 +20,13 @@ main(int argc, char **argv)
 {
     int     sockfd, n;
     char    recvline[MAXLINE + 1], hostname[MAXLINE];
-    // struct sockaddr_in servaddr;
     struct addrinfo hints, *servinfo, *p;
     struct sockaddr_in sa;
 
-    if (argc != 3) {
-        printf("usage: client <IPaddress> <Portnum>\n");
+    if (argc < 3 || argc == 4) {
+        printf("usage: client <Optional: Tunnel IP/Name> <Optional: Tunnel Port> <Server IP/Name> <Server Port>\n");
         exit(1);
     }
-
-    /*
-    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("socket error\n");
-        exit(1);
-    }
-
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(atoi(argv[2]));
-
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-        printf("inet_pton error for %s\n", argv[1]);
-        exit(1);
-    }
-
-    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-        printf("connect error\n");
-        exit(1);
-    }*/
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
@@ -80,7 +59,7 @@ main(int argc, char **argv)
         exit(1);
     }
 
-    /* get hostname */
+    /* get hostname of server */
     sa.sin_family = AF_INET;
     sa.sin_port = htons(atoi(argv[2]));
     if (inet_pton(AF_INET, argv[1], &sa.sin_addr) <= 0) {
